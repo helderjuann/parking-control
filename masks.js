ecd5a0d78a1988f15e5df31a5d9c31325e368fbe
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-07 17:14:49 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -2,3 +2,13 @@ $('.cpf').mask('000.000.000-00', {reverse: true});
 $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
 $('.carlicence').mask('AAA-0A00');
 $('.money').mask('000.000.000.000.000,00', {reverse: true});
+
+function disableTextS() {
+    $('body').css('-webkit-user-select','none');
+    $('body').css('-moz-user-select','none');
+    $('body').css('-ms-user-select','none');
+    $('body').css('-o-user-select','none');
+    $('body').css('user-select','none');
+}
+
+disableTextS();
