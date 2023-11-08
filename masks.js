1398fab1b27ddfa3684c98ef081524d039ce6e6c
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-11-08 18:39:20 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -5,6 +5,8 @@ $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
 $('.carlicence').mask('AAA-0A00');
 $('.money').mask('000.000.000.000.000,00', {reverse: true});
 
+// Desativar a seleção de texto
+
 function disableTextS() {
     $('body').css('-webkit-user-select','none');
     $('body').css('-moz-user-select','none');
